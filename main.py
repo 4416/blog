1001edd8214fffb3bbd4a7c3e913546206c4a330
Karim <directeur@gmail.com>
2008-11-26 01:08:46 +0100

    Added mp3url value to form when editing.

--- main.py
+++ main.py
@@ -329,6 +329,7 @@ class NewEntryHandler(BaseRequestHandler):
             except db.BadKeyError:
                 return self.redirect("/new")
         extra_context["form"] = form
+        extra_context["mp3url"] = entry.mp3url
         self.render("new.html", extra_context)
 
     @admin
